e6067202e881da8c7b939d808e87dd0e95ba02c4
saurabhvybs <psaurabh51@gmail.com>
2024-05-12 19:03:45 +0530

    Recursion prob

---
+++ Recursion/Basic_rec6.cpp
@@ -0,0 +1,19 @@
+// Program to print the sum of 1-N nos using Recursion.
+#include <iostream>
+using namespace std;
+
+int rec(int n,int sum){
+
+    if(n<=0){
+        return sum;
+    }
+    rec(n-1,sum+n);
+}
+int main(){
+    int n,sum=0;
+    cout<<"Enter the no: ";
+    cin>>n;
+   int ans= rec(n,sum);
+   cout<<"\nThe Sum is: "<<ans;
+   return 0;
+}
